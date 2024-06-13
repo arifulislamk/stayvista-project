a4977cc1a38ed028e2b746538ccb0c2a202d010a
arifulislamk <ariful18374@gmail.com>
2024-06-13 11:26:03 +0600

    live host in firebase and vercel

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ const port = process.env.PORT || 8000
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 
+    'http://localhost:5174',
+    'https://stayvista-3d83c.firebaseapp.com',
+    'https://stayvista-3d83c.web.app'
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 }
